a0174ecabee46df508e518134b16409e2694f4f5
Igor R. <124288137+Ig0rGon@users.noreply.github.com>
2025-02-04 10:44:50 -0500

    created array.js

---
+++ JavaScript_Array_Methods /array.js
@@ -0,0 +1,55 @@
+// JavaScript Array Methods Homework
+
+let superHeroes = 
+[
+    {name: "Superman", power: 100, universe: "DC"},
+    {name: "CaptainAmerica", power: 93, universe: "Marvel"},
+    {name: "Batman", power: 75, universe: "DC"},
+    {name: "BlackWidow", power: 78, universe: "Marvel"},
+    {name: "DrStrange", power: 91, universe: "Marvel"},
+    {name: "SpiderMan", power: 90, universe: "Marvel"},
+    {name: "IronMan", power: 88, universe: "Marvel"},
+    {name: "WonderWoman ", power: 82, universe: "DC"},
+    {name: "BlackPanther", power: 87, universe: "Marvel"},
+    {name: "Flash", power: 80, universe: "DC"},
+    {name: "Hulk", power: 83, universe: "Marvel"},
+    {name: "Thor", power: 95, universe: "Marvel"},
+]
+
+superHeroes.forEach(hero => {
+    if (hero.universe == "DC") {
+        hero.team = "Justice League"
+    } else {
+        hero.team = "Avengers"
+    }
+});
+
+let rankedSuperHeroes = superHeroes.map( rank => {
+    if (rank.power > 90) {
+        hero.rank = "Top Tier"
+    } else {
+        hero.rank ="Second Tier"
+    }
+})
+
+superHeroes.filter(hero.universe == "Marvel");
+
+superHeroes.sort(hero.rank);
+
+superHeroes.forEach(hero => {
+    if (hero.universe == "DC") {
+        hero.team = "Justice League"
+    } else {
+        hero.team = "Avengers"
+    }
+});
+
+superHeroes.forEach(hero => console.log(hero));
+
+rankedSuperHeroes.forEach(hero => console.log(hero));
+
+let marvelHeroes = rankedSuperHeroes.filter(hero => hero.universe == "Marvel");
+marvelHeroes.forEach(hero => console.log(hero));
+
+let sortedHeroes = superHeroes.sort((a, b) => b.power - a.power);
+sortedHeroes.forEach(hero => console.log(hero));
