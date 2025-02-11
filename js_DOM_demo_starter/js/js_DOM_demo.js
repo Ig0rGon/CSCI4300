e945e05f03823876da81357fb9d20a4d79d67d47
Igor R. <124288137+Ig0rGon@users.noreply.github.com>
2025-02-11 08:12:32 -0500

    added classActivity

---
+++ js_DOM_demo_starter/js/js_DOM_demo.js
@@ -0,0 +1,66 @@
+/*
+  switch the heading to 'I love JavaScript!
+  */
+
+
+// let's do that on an event, as a callback to setTimeout
+
+/*
+ switch the image
+ */
+//select the image by its id
+
+// switch the src attribute the other image
+
+// switch after timeout event
+
+
+/*
+ make the link href valid
+ */
+// select the element and store a reference in a variable.
+
+// change the text inside the link with the textContent property.
+
+// change the URL the link is pointing to:
+
+// get a reference to the div element node
+
+/* 
+Create a new paragraph element and give it some text
+*/
+
+// Now append the new paragraph at the end of the div
+
+// create a text node to the paragraph
+
+// Grab a reference to the paragraph the link is inside and append the text node to it.
+
+//
+// Moving and removing elements
+//
+// to move the paragraph with the link to the bottom of the div
+
+// to remove a node
+
+
+/*
+Add style
+*/
+// Can add inline styles directly as below
+/* 
+para.style.color = 'white';
+para.style.backgroundColor = 'black';
+para.style.padding = '10px';
+para.style.width = '250px';
+para.style.textAlign = 'center';
+*/
+// but better to add the style as a class and switch the class
+
+/*
+Build a list dynamically with javascript
+*/
+// create nodes to hold the list, input and button elements
+
+
+
