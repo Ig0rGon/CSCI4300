7e052e238052f07d170a774c201ea84acd876cfd
Igor R. <124288137+Ig0rGon@users.noreply.github.com>
2025-02-13 08:40:03 -0500

    classWork

---
+++ class_work/AL_jsdom.js
@@ -0,0 +1,11 @@
+function addHero() {
+    const inputField = document.querySelector("#heroInput");
+    const heroName = inputField.value;
+    const list = document.getElementById("heroList");
+    const listItem = document.createElement("li");
+    listItem.textContent = heroName;
+    list.appendChild(listItem);
+    inputField.value = "";
+}
+
+document.querySelector("#addHero").addEventListener("click", addHero);
